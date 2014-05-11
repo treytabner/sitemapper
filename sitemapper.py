3b49102d4eefab3c69aa1ef7ac7d050acd703955
Trey Tabner <trey@tabner.com>
2014-05-11 18:02:41 -0500

    Logging, more reliability

--- sitemapper.py
+++ sitemapper.py
@@ -15,38 +15,62 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 
 
-# setup log format...
+
 
 
 # exception types
 
 
-
 class Crawler():
-    def __init__(self, site):
+    def __init__(self, site, insecure=False):
         self.site = site
         self.sitemap = defaultdict(dict)
+        self.verify = not insecure
+
+    def check(self, item, attr):
+        value = item[attr]
+        if isinstance(value, six.string_types):
+            if re.compile("^/(?!/)").search(value):
+                return True
+            if value.startswith('mailto:'):
+                return False
+        if attr in ('href', 'src', 'action', ):
+            if re.compile("^http(?s)://").search(value):
+                return False
+            else:
+                return True
 
     def crawl(self, root='/'):
+        if root.startswith('/'):
+            url = '%s%s' % (self.site, root)
+        else:
+            url = '%s/%s' % (self.site, root)
+
+        r = requests.get(url, verify=self.verify)
+        if ((r.status_code != requests.codes.ok or
+             'text/html' not in r.headers['content-type'])):
+            return
+
         self.sitemap[root] = defaultdict(list)
-
-        r = requests.get(self.site + root)
-        if r.status_code is not requests.codes.ok:
-            raise Exception("Error fetching %s: %s" % (self.site, r))
-
         soup = BeautifulSoup(r.text)
         for i in soup.find_all():
             for attr in i.attrs:
-                if ((isinstance(i[attr], six.string_types) and
-                     re.compile("^/(?!/)").search(i[attr]))):
-                    if i[attr] not in self.sitemap[root][attr]:
-                        print i.name
-                        self.sitemap[root][attr].append(i[attr])
+                if self.check(i, attr):
+                    if i.name in ('a', 'form', ):
+                        key = 'links'
+                    else:
+                        key = 'assets'
 
-        return
-        for i in self.sitemap[root]['href']:
+                    what = re.compile("[\?#]").split(i[attr])[0]
+                    if what not in self.sitemap[root][key]:
+                        self.sitemap[root][key].append(what)
+        del soup, r
+
+        self.sitemap[root]['links'].sort()
+        self.sitemap[root]['assets'].sort()
+
+        for i in self.sitemap[root]['links']:
             if i not in self.sitemap:
-                print "Crawling %s" % i
                 self.crawl(root=i)
 
     def export(self):
@@ -55,15 +79,27 @@ class Crawler():
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--insecure', '-k', action='store_true',
+                        help="Disable SSL certificate verification")
+    parser.add_argument('--verbose', '-v', action='count',
+                        help="Enable verbose logging")
     parser.add_argument('site', help="Site to crawl and limit requests to")
-    # verify ssl
-    # verbose?
-    # version?
     # auth support?
 
     args = parser.parse_args()
 
-    crawler = Crawler(args.site)
+    if args.verbose == 1:
+        log_level = logging.INFO
+    elif args.verbose > 1:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.WARNING
+
+    log_format = "[%(asctime)s] %(module)s %(levelname)s: %(message)s"
+    logging.basicConfig(format=log_format, level=log_level,
+                        datefmt='%c', stream=sys.stdout)
+
+    crawler = Crawler(args.site, args.insecure)
     crawler.crawl()
     sitemap = crawler.export()
     print json.dumps(sitemap, sort_keys=True,
