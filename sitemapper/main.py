1ab1f9ae696cc6c32fc669dccd0dbd8e85125fc5
Trey Tabner <trey@tabner.com>
2014-05-11 21:01:59 -0500

    More tests

--- sitemapper/main.py
+++ sitemapper/main.py
@@ -15,6 +15,31 @@ import six
 from bs4 import BeautifulSoup
 
 
+def parse_args(args=sys.argv[1:]):
+    """Parse arguments, setup logging and execute sitemap generation"""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--exclude', '-x', action='append', default=[],
+                        help="Exclude URLs matching specified pattern")
+    parser.add_argument('--insecure', '-k', action='store_true',
+                        help="Disable SSL certificate verification")
+    parser.add_argument('--debug', '-d', action='store_true',
+                        help="Enable debug logging")
+    parser.add_argument('site', help="Site to crawl and limit requests to")
+    return parser.parse_args(args=args)
+
+
+def setup_logging(debug=False):
+    """Setup logging based on specified verbosity"""
+    if debug:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.WARNING
+
+    log_format = "[%(asctime)s] %(module)s %(levelname)s: %(message)s"
+    logging.basicConfig(format=log_format, level=log_level,
+                        datefmt='%c', stream=sys.stdout)
+
+
 def check_link(item, attr):
     """Check if we think the supplied item attribute is a local link"""
     value = item[attr]
@@ -40,38 +65,27 @@ def fix_site(site):
         else 'http://%s' % site
 
 
-def parse_args(args=sys.argv[1:]):
-    """Parse arguments, setup logging and execute sitemap generation"""
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--exclude', '-x', action='append', default=[],
-                        help="Exclude URLs matching specified pattern")
-    parser.add_argument('--insecure', '-k', action='store_true',
-                        help="Disable SSL certificate verification")
-    parser.add_argument('--verbose', '-v', action='count',
-                        help="Enable verbose logging")
-    parser.add_argument('site', help="Site to crawl and limit requests to")
-    return parser.parse_args(args=args)
+def fix_root(site, root):
+    """Return a normalized URL for the """
+    return '%s%s' % (site, root) if root.startswith('/') \
+        else '%s/%s' % (site, root)
 
 
-def setup_logging(verbosity):
-    """Setup logging based on specified verbosity"""
-    if verbosity == 1:
-        log_level = logging.INFO
-    elif verbosity > 1:
-        log_level = logging.DEBUG
-    else:
-        log_level = logging.WARNING
+def get_key(name):
+    """Return whether or not the specified item is a link or asset"""
+    return 'links' if name in ('a', 'form', ) else 'assets'
 
-    log_format = "[%(asctime)s] %(module)s %(levelname)s: %(message)s"
-    logging.basicConfig(format=log_format, level=log_level,
-                        datefmt='%c', stream=sys.stdout)
+
+def get_base(url):
+    """Return the base URL, with no GET parameters or anchors"""
+    return re.compile("[\\?#]").split(url)[0]
 
 
 class Crawler(object):
     """Crawler that fetches website content and generates a sitemap"""
     def __init__(self):
         self.args = parse_args()
-        setup_logging(self.args.verbose)
+        setup_logging(debug=self.args.debug)
 
         self.sitemap = collections.defaultdict(dict)
         self.verify = not self.args.insecure
@@ -79,30 +93,28 @@ class Crawler(object):
 
     def generate(self, root='/'):
         """Crawl provided website, generating sitemap as we go"""
-        if root.startswith('/'):
-            url = '%s%s' % (self.site, root)
-        else:
-            url = '%s/%s' % (self.site, root)
+        url = fix_root(self.site, root)
 
+        # Fetch content
         response = requests.get(url, verify=self.verify)
+
+        # Return if not an HTTP 200 or not text/html
         if ((response.status_code != requests.codes['ok'] or
              'text/html' not in response.headers['content-type'])):
             return
 
         self.sitemap[root] = collections.defaultdict(list)
+
+        # Parse the HTML
         soup = BeautifulSoup(response.text)
         for i in soup.find_all():
             for attr in i.attrs:
                 if check_link(i, attr):
-                    if i.name in ('a', 'form', ):
-                        key = 'links'
-                    else:
-                        key = 'assets'
-
-                    what = re.compile("[\\?#]").split(i[attr])[0]
-                    if what not in self.sitemap[root][key]:
-                        if True not in [x in what for x in self.args.exclude]:
-                            self.sitemap[root][key].append(what)
+                    key = get_key(i.name)
+                    base = get_base(i[attr])
+                    if base not in self.sitemap[root][key]:
+                        if True not in [x in base for x in self.args.exclude]:
+                            self.sitemap[root][key].append(base)
         del soup, response
 
         self.sitemap[root]['links'].sort()
--- tests/test_sitemapper.py
+++ tests/test_sitemapper.py
@@ -1,14 +1,34 @@
 """Tests for sitemapper: Python based crawler and sitemap generator"""
 
 
-def test_fix_site():
-    """Tests for sitemapper.main.fix_site"""
-    from sitemapper.main import fix_site
+def test_parse_args():
+    """Tests for sitemapper.main.parse_args"""
+    from shlex import split
+    from sitemapper.main import parse_args
 
-    assert fix_site('treytabner.com') == 'http://treytabner.com'
-    assert fix_site('http://treytabner.com') == 'http://treytabner.com'
-    assert fix_site('https://treytabner.com') == 'https://treytabner.com'
-    assert fix_site('ssh://treytabner.com') == 'ssh://treytabner.com'
+    args = parse_args(split("www.example.com"))
+    assert args.site == 'www.example.com'
+    assert args.insecure is False
+    assert args.debug is False
+    assert args.exclude == []
+
+    args = parse_args(split("--insecure https://www.example.com"))
+    assert args.site == 'https://www.example.com'
+    assert args.insecure is True
+    assert args.debug is False
+    assert args.exclude == []
+
+    args = parse_args(split("-d --exclude test http://www.example.com"))
+    assert args.site == 'http://www.example.com'
+    assert args.insecure is False
+    assert args.debug is True
+    assert args.exclude == ['test']
+
+    args = parse_args(split("--debug -x test1 -x test2 example.com"))
+    assert args.site == 'example.com'
+    assert args.insecure is False
+    assert args.debug is True
+    assert args.exclude == ['test1', 'test2']
 
 
 def test_check_link():
@@ -29,31 +49,41 @@ def test_check_link():
     assert check_link({'value': 'submit'}, 'value') is False
 
 
-def test_parse_args():
-    """Tests for sitemapper.main.parse_args"""
-    from shlex import split
-    from sitemapper.main import parse_args
+def test_fix_site():
+    """Tests for sitemapper.main.fix_site"""
+    from sitemapper.main import fix_site
 
-    args = parse_args(split("www.example.com"))
-    assert args.site == 'www.example.com'
-    assert args.insecure is False
-    assert args.verbose is None
-    assert args.exclude == []
+    assert fix_site('treytabner.com') == 'http://treytabner.com'
+    assert fix_site('http://treytabner.com') == 'http://treytabner.com'
+    assert fix_site('https://treytabner.com') == 'https://treytabner.com'
+    assert fix_site('ssh://treytabner.com') == 'ssh://treytabner.com'
 
-    args = parse_args(split("--insecure -v https://www.example.com"))
-    assert args.site == 'https://www.example.com'
-    assert args.insecure is True
-    assert args.verbose is 1
-    assert args.exclude == []
 
-    args = parse_args(split("-vv --exclude test http://www.example.com"))
-    assert args.site == 'http://www.example.com'
-    assert args.insecure is False
-    assert args.verbose is 2
-    assert args.exclude == ['test']
+def test_fix_root():
+    """Tests for sitemapper.main.fix_root"""
+    from sitemapper.main import fix_root
 
-    args = parse_args(split("--verbose -x test1 -x test2 example.com"))
-    assert args.site == 'example.com'
-    assert args.insecure is False
-    assert args.verbose is 1
-    assert args.exclude == ['test1', 'test2']
+    assert (fix_root('http://example.com', '/contact') ==
+            'http://example.com/contact')
+    assert (fix_root('http://example.com', 'contact') ==
+            'http://example.com/contact')
+
+
+def test_get_key():
+    """Tests for sitemapper.main.get_key"""
+    from sitemapper.main import get_key
+
+    assert get_key('a') == 'links'
+    assert get_key('form') == 'links'
+    assert get_key('img') == 'assets'
+    assert get_key('link') == 'assets'
+    assert get_key('script') == 'assets'
+
+
+def test_get_base():
+    """Tests for sitemapper.main.get_base"""
+    from sitemapper.main import get_base
+
+    assert get_base('/example') == '/example'
+    assert get_base('/example#footer') == '/example'
+    assert get_base('/example?test=1') == '/example'
